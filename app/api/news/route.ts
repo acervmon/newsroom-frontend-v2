03ab9c3d2a10b0f0244674a424b32cfe849dc208
acervmon <126948783+acervmon@users.noreply.github.com>
2026-02-23 22:56:33 +0100

    Ni me pagan SOS

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,3 +1,4 @@
+export const runtime = "nodejs";
 // app/api/news/route.ts
 import { NextResponse } from "next/server";
 import {
--- lib/s3.ts
+++ lib/s3.ts
@@ -1,26 +1,5 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
+  region: process.env.AWS_REGION || "eu-north-1",
 });
-
-export const uploadFile = async (
-  fileName: string,
-  fileContent: Buffer,
-  mimeType: string
-) => {
-  const command = new PutObjectCommand({
-    Bucket: process.env.S3_BUCKET,
-    Key: fileName,
-    Body: fileContent,
-    ContentType: mimeType,
-  });
-
-  await s3.send(command);
-
-  return `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
-};
